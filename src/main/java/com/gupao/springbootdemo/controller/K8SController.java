c654776363ef8af9b118b1e28774f4f6dae41b4c
svenzt <46935030@qq.com>
2021-12-27 16:06:53 +0800

    second

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -12,6 +12,6 @@ public class K8SController {
 
     @RequestMapping("/k8s")
     public String k8s(){
-        return "hello K8s <br/>111222 ";
+        return "hello K8s <br/>111222 sven ";
     }
 }
